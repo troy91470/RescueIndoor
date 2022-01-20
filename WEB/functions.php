85fd520a902ae3938b20698d4193cd3415db1278
Marwane <bahraoui.marwane@gmail.com>
2022-01-20 17:51:19 +0100

    func

--- WEB/functions.php
+++ WEB/functions.php
@@ -12,6 +12,7 @@ function test()
 	$gw_databaseName = 'appliprojetinterfiliere';
 
 function bd_connexion(){
+    require("logs.php");
     $connexionBDD = new mysqli($servername,$username,$password);
     mysqli_select_db($connexionBDD, $gw_databaseName);
     // Vérifier la connexion
@@ -24,9 +25,28 @@ function bd_connexion(){
 function suppression_employe($first_name, $second_name, $password, $office)
 {
     $connexionBDD = bd_connexion();
-    
+    echo "fn: $first_name, sn: $second_name , pswd: $password";
     mysqli_close();
 }
 
+function verification_same_person($first_name, $second_name){
+    $connexionBDD = bd_connexion();
+    $requeteSelectSamePerson = "SELECT count(*) FROM user WHERE first_name='".$first_name."' and second_name='".$second_name."'";
+    $result = $connexionBDD -> query($requeteSelectSamePerson);
+    $result = $result -> fetch_array();
+    return (bool) ($result[0]);
+}
+
+function modification_employe($first_name, $second_name, $password, $office)
+{
+    $existsAlready = verification_same_person($first_name, $second_name);
+    if($existsAlready){
+        echo "<script>alert(\"Cette personne existe déjà dans la base de donnée.\")</script>";
+    }
+    else{
+        echo "ça passe !";
+    }
+    mysqli_close();
+}
 
 ?>
