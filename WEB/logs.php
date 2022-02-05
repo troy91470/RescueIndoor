dd76a4c25840fe075dec979ca3facd629fcbeeb1
Marwane <bahraoui.marwane@gmail.com>
2022-02-05 11:15:41 +0100

    add logs ros

--- WEB/logs.php
+++ WEB/logs.php
@@ -5,6 +5,7 @@
 	$password = '';
 	$gw_databaseName = 'appliprojetinterfiliere';
 	$ros_ip = '192.168.166.7';
+	$ros_bridge = 'ws://192.168.166.7:9090';
 	$ros_username = 'ros';
 	$ros_password = 'ros';
 ?>
