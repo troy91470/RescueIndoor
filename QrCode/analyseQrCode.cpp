007b71cac4a17c9c39357b1d83a689dbe6e92aaf
Marwane <bahraoui.marwane@gmail.com>
2022-03-24 13:30:29 +0100

    check

--- QrCode/analyseQrCode.cpp
+++ QrCode/analyseQrCode.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	else
 		inputImage = imread("png_random.pngg");
 
-	QRCodeDetector qrDecoder = QRCodeDetector();
+	QRCodeDetector qrDecoder = QRCodeDetector::QRCodeDetector();
 
 	Mat bbox, rectifiedImage;
 
