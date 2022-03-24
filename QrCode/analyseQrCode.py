8131aa691bb0a93039713af11c72849b78405515
Marwane <bahraoui.marwane@gmail.com>
2022-03-24 15:08:05 +0100

    maj

--- QrCode/analyseQrCode.py
+++ QrCode/analyseQrCode.py
@@ -36,6 +36,27 @@ cap = cv2.VideoCapture(0)
 # QR code detection object
 detector = cv2.QRCodeDetector()
 
+
+def listeBureauxPublisher():
+    pub = rospy.Publisher('listeTopic', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(1) # 1hz
+    while not rospy.is_shutdown():
+        _, img = cap.read()
+        data, bbox, _ = detector.detectAndDecode(img) # data = numero  bu
+        if(bbox is not None):
+            if data:
+                if memo != data :
+                    memo = data
+                    rospy.loginfo(data)
+                    pub.publish(data)
+                    rate.sleep()
+if __name__ == '__main__':
+    try:
+        listeBureauxPublisher()
+    except rospy.ROSInterruptException:
+        pass
+
 while True:
     # get the image
     _, img = cap.read()
