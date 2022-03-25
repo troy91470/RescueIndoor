78bf88cd2796c368d7f3a55142174e7fa384fdc1
Marwane <bahraoui.marwane@gmail.com>
2022-03-25 11:15:55 +0100

    correction qrcode sleep

--- Ros/src/carteIno/analyseQrCode.py
+++ Ros/src/carteIno/analyseQrCode.py
@@ -4,13 +4,15 @@ import cv2
 import rospy
 from std_msgs.msg import String
 
+
+
 def listeBureauxPublisher():
     memo = ""
     # set up camera object
     cap = cv2.VideoCapture(0)
     # QR code detection object
     detector = cv2.QRCodeDetector()
-    pub = rospy.Publisher('qrCodeData', String, queue_size=10)
+    pub = rospy.Publisher('listeTopic', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(1) # 1hz
     while not rospy.is_shutdown():
@@ -20,10 +22,9 @@ def listeBureauxPublisher():
             if data:
                 if memo != data :
                     memo = data
-                    bureau = data.split()
-                    rospy.loginfo(bureau[0])
-                    pub.publish(bureau[0])
-                    rate.sleep()
+                    rospy.loginfo(data)
+                    pub.publish(data)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
